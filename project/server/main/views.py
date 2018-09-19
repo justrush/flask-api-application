8e072efd37b3c25c7ab5295a37485f5c45521b95
justinrush <justinrushwa@gmail.com>
2018-09-18 20:59:25 -0700

    v1_1_add_bitstamp
    Signed-off-by: justinrush <justinrushwa@gmail.com>

--- project/server/main/views.py
+++ project/server/main/views.py
@@ -12,6 +12,11 @@ def home():
     bitstamp_ticker_shit = get_bitstamp_ticker()
     return render_template('main/home.html', bitstamp_last_price=bitstamp_ticker_shit)
 
+@main_blueprint.route('/')
+def home():
+    bitstamp_ticker_shit = get_bitstamp_ticker()
+    return render_template('main/home.html', bitstamp_last_price=bitstamp_ticker_shit)
+
 @main_blueprint.route("/about/")
 def about():
     return render_template("main/about.html")
